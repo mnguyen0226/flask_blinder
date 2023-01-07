e6fd7c930b3149733ba07e6f5cfe92a029448c27
mnguyen0226 <mnguyen0226@vt.edu>
2023-01-07 13:26:56 -0500

    create simple user page

--- src/main.py
+++ src/main.py
@@ -7,8 +7,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello"
+    return render_template("index.html")
 
+@app.route("/user/")
+def user():
+    name_list = ["Minh", "Tam", "Alex"]
+    return render_template("user.html", user_name=name_list)
 
 if __name__ == "__main__":
     app.run(debug=True)
