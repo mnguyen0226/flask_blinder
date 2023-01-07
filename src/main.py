83bf687dca9066735f35ad3d965c7704c67c036e
mnguyen0226 <mnguyen0226@vt.edu>
2023-01-07 13:54:05 -0500

    add error pages

--- src/main.py
+++ src/main.py
@@ -4,15 +4,26 @@ from flask import render_template
 # create a flask project
 app = Flask(__name__)
 
-
+# home page
 @app.route("/")
 def index():
     return render_template("index.html")
 
+# user page
 @app.route("/user/")
 def user():
     name_list = ["Minh", "Tam", "Alex"]
     return render_template("user.html", user_name=name_list)
+
+# invalid url page
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+# internal server error page
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
 
 if __name__ == "__main__":
     app.run(debug=True)
