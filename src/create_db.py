1fa22d62d051a873b6984ce4df863aaa58fa6fe7
mnguyen0226 <mnguyen0226@vt.edu>
2023-01-19 21:13:13 -0500

    create database

---
+++ src/create_db.py
@@ -0,0 +1,20 @@
+import mysql.connector
+
+# connect to local database
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="password123",
+    auth_plugin="mysql_native_password",
+)
+
+# object interact with database
+my_cursor = mydb.cursor()
+
+# create new database
+my_cursor.execute("CREATE DATABASE flask_hacker_news")
+
+# show all database
+my_cursor.execute("SHOW DATABASES")
+for db in my_cursor:
+    print(db)
