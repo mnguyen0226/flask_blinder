3e73c7c6e21f2a40dfc7733c265f7514ba40aa7b
mnguyen0226 <mnguyen0226@vt.edu>
2023-01-19 21:18:18 -0500

    run black

--- src/app.py
+++ src/app.py
@@ -47,11 +47,13 @@ ckeditor = CKEditor(app)
 def load_user(user_id):
     return Users.query.get(int(user_id))
 
+
 # link form from base.html to navbar search bar
 @app.context_processor
 def base_link_form():
     form = SearchForm()
     return dict(form=form)
+
 
 #############################################
 # ROUTES
@@ -72,82 +74,84 @@ def page_not_found(e):
 @app.route("/sign_up", methods=["GET", "POST"])
 def sign_up():
     form = UserForm()
-    
+
     # TODO: move this list to admin page
     users_list = Users.query.order_by(Users.date_added)
-    
+
     # post request
     if request.method == "POST":
-        
-        # if the user fill out the form correctly 
+
+        # if the user fill out the form correctly
         if form.validate_on_submit():
-            
+
             # search the user by email
             user = Users.query.filter_by(email=form.email.data).first()
-            
+
             # if a new user, add to the database
             if user is None:
-                
+
                 hashed_pw = generate_password_hash(form.password_create.data, "sha256")
-                
+
                 new_user = Users(
                     name=form.name.data,
                     username=form.username.data,
                     email=form.email.data,
                     hashed_password=hashed_pw,
                 )
-                
+
                 db.session.add(new_user)
                 db.session.commit()
                 flash("New user signed up successfully!")
 
             else:
                 flash("Error: This email has been taken. Please choose another email!")
-            
-            # reset the form 
+
+            # reset the form
             form.name.data = ""
             form.username.data = ""
             form.email.data = ""
             form.password_create.data = ""
             form.password_confirm.data = ""
-        
+
         # update the new database
         users_list = Users.query.order_by(Users.date_added)
         return render_template("sign_up.html", form=form, users_list=users_list)
-    
+
     # get request
     else:
         return render_template("sign_up.html", form=form, users_list=users_list)
+
 
 @app.route("/delete/<int:id>")
 def delete(id):
     # get user from id
     user_to_delete = Users.query.get_or_404(id)
     form = UserForm()
-    
+
     try:
         db.session.delete(user_to_delete)
         db.session.commit()
         flash("User deleted successfully!")
-        
+
         # update user list
         users_list = Users.query.order_by(Users.date_added)
         return render_template("sign_up.html", form=form, users_list=users_list)
-    
+
     except:
-        flash("Error: Unable to delete user as the user does not exist!")            
+        flash("Error: Unable to delete user as the user does not exist!")
 
         users_list = Users.query.order_by(Users.date_added)
         return render_template("sign_up.html", form=form, users_list=users_list)
-    
-# login 
+
+
+# login
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
-    
-    # if the user fill out the form correctly 
+
+    # if the user fill out the form correctly
     if form.validate_on_submit():
-        
+
         # query the user base on username
         found_username = Users.query.filter_by(username=form.username.data).first()
 
@@ -157,39 +161,44 @@ def login():
                 flash("Login successfully!")
                 login_user(found_username)
                 return redirect(url_for("dashboard"))
-            
+
             else:
                 flash("Error: Wrong password. Try again!")
         else:
             flash("Error: Username does not exist. Try again!")
-    return render_template("login.html", form=form)            
-    
+    return render_template("login.html", form=form)
+
+
 # user dashboard
-@app.route("/dashboard", methods=["GET","POST"])
+@app.route("/dashboard", methods=["GET", "POST"])
 @login_required
 def dashboard():
     form = UserForm()
     id = current_user.id
-    name_to_update = Users.query.get_or_404(id) 
-    
+    name_to_update = Users.query.get_or_404(id)
+
     # post
     if request.method == "POST":
         name_to_update.name = request.form["name"]
         name_to_update.email = request.form["email"]
         name_to_update.username = request.form["username"]
-        
+
         try:
             db.session.commit()
             flash("Profile updated successfully!")
-            return render_template("dashboard.html", form=form, name_to_update=name_to_update)
+            return render_template(
+                "dashboard.html", form=form, name_to_update=name_to_update
+            )
         except:
             flash("Error: Can't update. Try again!")
             return render_template(
                 "dashboard.html", form=form, name_to_update=name_to_update
             )
     else:
-        return render_template("dashboard.html", form=form, name_to_update=name_to_update)
-       
+        return render_template(
+            "dashboard.html", form=form, name_to_update=name_to_update
+        )
+
 
 #############################################
 # DATABASES
